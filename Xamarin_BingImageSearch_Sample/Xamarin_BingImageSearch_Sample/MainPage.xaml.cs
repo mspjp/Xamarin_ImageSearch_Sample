d9587c237e0a3ad15a7bc7b0801c0479798b2559
Fumiya Kume <fumiya.kume@hotmail.com>
2017-06-24 00:41:16 +0900

    Init Commit

---
+++ Xamarin_BingImageSearch_Sample/Xamarin_BingImageSearch_Sample/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace Xamarin_BingImageSearch_Sample
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<ImageData> ImageDataList { get; set; } = new ObservableCollection<ImageData>();
+        public MainPage()
+        {
+            InitializeComponent();
+            ListView.ItemsSource = ImageDataList;
+        }
+
+        private async void SearchButton_Clicked(object sender, EventArgs e)
+        {
+            var key = BingKey.Text;
+            var Searchtext = SearchText.Text;
+            foreach (var imageData in await BingClient.GetBingImageSeatchAsync(Searchtext, key))
+            {
+                ImageDataList.Add(imageData);
+            }
+        }
+    }
+}
